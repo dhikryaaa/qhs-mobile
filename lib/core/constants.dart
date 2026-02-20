56f8bcd5d1d4b9bd5c702647ee656f61bf7c3205
Andhika Aryaputra <andhika.aryaputra@gmail.com>
2026-02-20 18:42:55 +0700

    refactor: using ngrok for mobile link (DEV ONLY)

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -4,6 +4,6 @@ String baseUrl() {
   if (kIsWeb) {
     return 'http://localhost:8000';
   } else {
-    return 'http://10.0.2.2:8000';
+    return 'https://pervertible-unvolubly-theressa.ngrok-free.dev';
   }
 }
